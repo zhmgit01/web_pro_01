db786f3cf77555bc36ea4cbbec66d9b82560eccb
zhm <2964502580@qq.com>
2020-10-23 11:20:08 +0800

    添加allure的使用

--- run.py
+++ run.py
@@ -3,6 +3,14 @@
 # @Time      :2020/10/21 9:28
 # @Author    :zhm
 
+import os
 import pytest
 
-pytest.main()
+# 1、生成alluer测试报告数据
+pytest.main(["--alluredir=test_results/reports",  # 指定生产allure报告的路径
+             '--reruns', '3',  # 指定失败重运行的次数
+             '--reruns-delay', '2'  # 指定失败重运行的间隔时间
+             ])
+
+# 2、启动allure服务
+# os.system('allure serve test_result/reports')
